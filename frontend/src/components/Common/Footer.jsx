6690bb3626934d03c57776c430439ab1e77c6c6e
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-04-24 19:08:01 +0530

    add container structure to Footer component

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -8,7 +8,9 @@ import { FiPhoneCall } from 'react-icons/fi';
 const Footer = () => {
   return (
     <footer className="border-t py-12">
-     
+     <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0"> 
+        
+     </div>
            </footer>
   );
 };

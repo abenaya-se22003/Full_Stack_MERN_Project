0f31922ff14236f9ed04c21c77999b4e0d317512
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-04-29 17:49:56 +0530

    implement AdminLayout with sidebar

--- frontend/src/components/Admin/AdminLayout.jsx
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import { FaBars, FaBoxOpen, FaClipboardList, FaUser, FaStore } from "react-icons/fa";
+import { Link, Outlet } from "react-router-dom";
 
-function AdminLayout() {
+const AdminLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div>AdminLayout</div>
-  )
-}
+    <div className="min-h-screen flex flex-col md:flex-row relative">
+      {/* Mobile Toggle Button */}
+      <div className="flex md:hidden p-4 bg-gray-900 text-white z-20 items-center justify-between">
+        <button onClick={toggleSidebar}>
+          <FaBars size={24} />
+        </button>
+        <h1 className="ml-4 text-xl font-medium">Admin Dashboard</h1>
+      </div>
 
-export default AdminLayout
+      {/* --- Sidebar --- */}
+     
+
+      {/* --- Main Content --- */}
+      <div className="flex-grow p-6 overflow-y-auto bg-gray-50">
+        {/* The Outlet is where different admin pages (Products, Orders, etc.) will appear */}
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;

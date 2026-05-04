7fa5e64f0b399955baffb0d191e122b75de5843a
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-05-04 11:22:52 +0530

    create order management page

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Product = require('../models/Product');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+
+// @route POST /api/products
+// @desc Create a new product
+// @access Private (Admin only)

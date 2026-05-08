d3bc3f2da5f20953cb4f6dda5a9f54b5c5d49931
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-05-08 12:50:01 +0530

    setup counter rout

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -85,4 +85,47 @@ router.post('/', async (req, res) => {
     }
 });
 
+// @route PUT /api/cart
+// @desc Update product quantity in the cart for a guest or logged-in user
+// @access Public
+router.put("/", async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color
+    );
+
+    if (productIndex > -1) {
+      // update quantity
+      if (quantity > 0) {
+        cart.products[productIndex].quantity = quantity;
+      } else {
+        // Remove product if quantity is 0 or less
+        cart.products.splice(productIndex, 1);
+      }
+
+      // Recalculate total price
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;

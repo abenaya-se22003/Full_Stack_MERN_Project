eefbe7602397f864b6894686349af68e40e01e45
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-05-13 11:53:22 +0530

    add new user function

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -20,4 +20,29 @@ router.get("/", protect, admin, async (req, res) => {
     }
 });
 
+// @route POST /api/admin/users
+// @desc Create a new user (Admin only)
+// @access Private/Admin
+router.post("/", protect, admin, async (req, res) => {
+    const { name, email, password, role } = req.body;
+
+    try {
+        // Check if the email is already in use
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "Email is already in use" });
+        }
+
+        // Create a new user
+        user = new User({ name, email, password,role: role || "customer",});
+        await user.save();
+
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.error("Admin Create User Error:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
+
 module.exports = router;
